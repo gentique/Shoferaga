facf51a3d357ffaae7ce6cd35819e510162e6d20
Gentian Barileva <gentianbarileva@gmail.com>
2018-05-16 22:56:31 +0200

    initial commit

---
+++ ShoferagaTests/ShoferagaTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShoferagaTests.swift
+//  ShoferagaTests
+//
+//  Created by Gentian Barileva on 5/16/18.
+//  Copyright © 2018 Gentian Barileva. All rights reserved.
+//
+
+import XCTest
+@testable import Shoferaga
+
+class ShoferagaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
